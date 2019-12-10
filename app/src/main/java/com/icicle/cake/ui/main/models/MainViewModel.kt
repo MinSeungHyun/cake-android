11b993a2ea33958ffbb294e9e72dcdebd5e9b182
SeungHyunMin <asdf014563@naver.com>
2019-12-10 21:42:10 +0900

    튜토리얼 보기 버튼 구현

--- app/src/main/java/com/icicle/cake/ui/main/models/MainViewModel.kt
+++ app/src/main/java/com/icicle/cake/ui/main/models/MainViewModel.kt
@@ -1,9 +1,11 @@
 package com.icicle.cake.ui.main.models
 
+import android.content.Intent
 import android.util.Log
 import androidx.databinding.ObservableArrayList
 import com.icicle.cake.R
 import com.icicle.cake.ui.main.MainActivity
+import com.icicle.cake.ui.tutorial.TutorialActivity
 import com.icicle.cake.util.QRReader
 
 class MainViewModel(private val activity: MainActivity) {
@@ -15,5 +17,9 @@ class MainViewModel(private val activity: MainActivity) {
 
     fun onCodeScanned(content: String) {
         Log.d("testing", content)
+    }
+
+    fun onHelpButtonClick() {
+        activity.startActivity(Intent(activity, TutorialActivity::class.java))
     }
 }
