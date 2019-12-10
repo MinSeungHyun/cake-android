5a03a897de5dc741a7c161600b08c4ed22a43e19
SeungHyunMin <asdf014563@naver.com>
2019-12-10 21:37:11 +0900

    튜토리얼 끝내기 버튼 구현

--- app/src/main/java/com/icicle/cake/ui/tutorial/TutorialActivity.kt
+++ app/src/main/java/com/icicle/cake/ui/tutorial/TutorialActivity.kt
@@ -1,11 +1,22 @@
 package com.icicle.cake.ui.tutorial
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager.widget.ViewPager
 import com.icicle.cake.R
 import kotlinx.android.synthetic.main.activity_tutorial.*
 
-class TutorialActivity : AppCompatActivity() {
+val tutorialImageIds = listOf(
+    R.drawable.tutorial_1,
+    R.drawable.tutorial_2,
+    R.drawable.tutorial_3,
+    R.drawable.tutorial_4,
+    R.drawable.tutorial_5,
+    R.drawable.tutorial_6
+)
+
+class TutorialActivity : AppCompatActivity(), ViewPager.OnPageChangeListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tutorial)
@@ -13,6 +24,17 @@ class TutorialActivity : AppCompatActivity() {
         viewPager.apply {
             clipToPadding = false
             adapter = TutorialPagerAdapter(this@TutorialActivity)
+            addOnPageChangeListener(this@TutorialActivity)
         }
+
+        startButton.setOnClickListener { finish() }
     }
+
+    override fun onPageSelected(position: Int) {
+        if (position == tutorialImageIds.size - 1) startButton.visibility = View.VISIBLE
+        else startButton.visibility = View.INVISIBLE
+    }
+
+    override fun onPageScrollStateChanged(state: Int) {}
+    override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {}
 }
--- app/src/main/java/com/icicle/cake/ui/tutorial/TutorialPagerAdapter.kt
+++ app/src/main/java/com/icicle/cake/ui/tutorial/TutorialPagerAdapter.kt
@@ -8,15 +8,6 @@ import androidx.viewpager.widget.PagerAdapter
 import com.icicle.cake.R
 import kotlinx.android.synthetic.main.item_tutorial_card.view.*
 
-private val tutorialImageIds = listOf(
-    R.drawable.tutorial_1,
-    R.drawable.tutorial_2,
-    R.drawable.tutorial_3,
-    R.drawable.tutorial_4,
-    R.drawable.tutorial_5,
-    R.drawable.tutorial_6
-)
-
 class TutorialPagerAdapter(private val context: Context) : PagerAdapter() {
     override fun instantiateItem(container: ViewGroup, position: Int): Any {
         val inflater = LayoutInflater.from(context)
