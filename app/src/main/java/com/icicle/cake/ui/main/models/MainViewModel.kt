b5660ad57b8265a2a30ffd5289ba548818fee3ed
SeungHyunMin <asdf014563@naver.com>
2019-12-11 18:11:01 +0900

    SwipeRefreshLayout 적용

--- app/src/main/java/com/icicle/cake/ui/main/models/MainViewModel.kt
+++ app/src/main/java/com/icicle/cake/ui/main/models/MainViewModel.kt
@@ -3,6 +3,7 @@ package com.icicle.cake.ui.main.models
 import android.content.Intent
 import android.util.Log
 import androidx.databinding.ObservableArrayList
+import androidx.databinding.ObservableBoolean
 import com.icicle.cake.R
 import com.icicle.cake.ui.main.MainActivity
 import com.icicle.cake.ui.tutorial.TutorialActivity
@@ -10,6 +11,7 @@ import com.icicle.cake.util.QRReader
 
 class MainViewModel(private val activity: MainActivity) {
     val reservationItems = ObservableArrayList<ReservationItem>()
+    val isRefreshing = ObservableBoolean().apply { set(false) }
 
     fun onCameraButtonClick() {
         QRReader.startReader(activity, R.string.scan_qr)
@@ -21,5 +23,10 @@ class MainViewModel(private val activity: MainActivity) {
 
     fun onHelpButtonClick() {
         activity.startActivity(Intent(activity, TutorialActivity::class.java))
+    }
+
+    fun onRefresh() {
+        isRefreshing.set(true)
+        isRefreshing.set(false)
     }
 }
