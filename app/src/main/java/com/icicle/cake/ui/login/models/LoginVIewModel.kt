f26f825cff8e4a1d3e29bd3ea5762f6574ba3b04
SeungHyunMin <asdf014563@naver.com>
2019-12-11 20:43:54 +0900

    LoginActivity ProgressBar 구현

--- app/src/main/java/com/icicle/cake/ui/login/models/LoginVIewModel.kt
+++ app/src/main/java/com/icicle/cake/ui/login/models/LoginVIewModel.kt
@@ -2,9 +2,12 @@ package com.icicle.cake.ui.login.models
 
 import android.app.Activity
 import android.content.Intent
+import androidx.databinding.ObservableBoolean
 import com.icicle.cake.ui.main.MainActivity
 
 class LoginVIewModel(private val activity: Activity) {
+    val isLoginProgressive = ObservableBoolean().apply { set(false) }
+
     fun onLoginButtonClick() {
         activity.startActivity(Intent(activity, MainActivity::class.java))
         activity.finish()
